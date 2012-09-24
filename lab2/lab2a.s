467a9b47a311990181be7a261b8074a0b2d69f96
Preston Maness <aggroskater@gmail.com>
2012-09-23 21:30:07 -0500

    updating requirements for lab2a

--- lab2/lab2a.s
+++ lab2/lab2a.s
@@ -5,5 +5,11 @@
 ;Date: Fall 2012 (Sep. 23 2012)
 
 ; this program needs to perform the following operations:
-; 
+; initialize array of 18 byte-size integers
+; add elements of array and store result in SUM
+; find average of numbers and store result in AVG
+; find maximum integer and store in MAX
+; find minimum integer and store in MIN
+
+ 
 
