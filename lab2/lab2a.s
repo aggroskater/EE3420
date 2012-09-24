057864115bb7e219ebb2cc85f9750ccbf0628305
Preston Maness <aggroskater@gmail.com>
2012-09-23 21:57:16 -0500

    working working working

--- lab2/lab2a.s
+++ lab2/lab2a.s
@@ -1,4 +1,4 @@
-;File: lab2a.asm
+;File: lab2a.s
 ;Author: Preston Maness
 ;Class: EE3420
 ;Professor: Dr. Salamy
@@ -11,5 +11,23 @@
 ; find maximum integer and store in MAX
 ; find minimum integer and store in MIN
 
- 
+#include "../EE3420.INC"
+
+	ORG $1000
+
+SUM DC.W 0		; sum of array
+AVG DC.W 0		; avg of array
+MAX DC.B 0		; max elem
+MIN DC.B 0		; min elem
+;ARRAY DCB.B 18,0	; array of 18 bytes initialized to zero
+ARRAY DC.B 1,6,2,4,4,5,3,3,2,1,4,2,6,3,5,4,4,4
+			; chosen by fair dice role. Guaranteed to be random.
+
+; need to initialize array with random values
+
+;GEN_RAND:
+
+; code to generate random number. gen on alloc A and st at mem
+; so yeah... this could be a project all by itself...
+
 
